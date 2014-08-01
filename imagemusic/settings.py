13113e405b9c5e0c42f2db76fd5c07321f94581f
kremerdesign <kremerdesign@gmail.com>
2014-08-01 16:42:55 -0700

    troubleshoot

--- imagemusic/settings.py
+++ imagemusic/settings.py
@@ -79,6 +79,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 
+
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATICFILES_DIRS = (
@@ -143,17 +144,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-# Static asset configuration
-PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static'),
-)
-
-
-STATIC_MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
-
-
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "static", *MEDIA_URL.strip("/").split("/"))
