2e0f7f1a723099730ca1413ea0d08b6a1922702d
kremerdesign <kremerdesign@gmail.com>
2014-08-01 17:09:52 -0700

    commenting out static settings

--- imagemusic/settings.py
+++ imagemusic/settings.py
@@ -24,7 +24,7 @@ DEBUG = False
 
 TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -82,15 +82,15 @@ USE_TZ = True
 
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
+# STATIC_URL = '/static/'
 
 # MEDIA_URL = '/media/'
 #
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static-only')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static-only')
 STATIC_MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 
