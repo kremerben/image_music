a0820c5c564ba7303ec671bdb5e58861ed55813d
kremerdesign <kremerdesign@gmail.com>
2014-08-02 14:51:19 -0700

    troubleshooting deployment

--- imagemusic/settings.py
+++ imagemusic/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'south',
     'photosong',
     'requests',
-    'debug_toolbar',
+    # 'debug_toolbar',
 
 )
 
@@ -142,3 +142,25 @@ try:
     from local_settings import *
 except ImportError:
     pass
+
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
